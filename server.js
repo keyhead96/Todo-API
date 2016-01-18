614b5ce1cd1e3c183b5af5b980fd3f35639292d1
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-18 06:16:47 +0000

    Fixed Mistake

--- server.js
+++ server.js
@@ -20,28 +20,7 @@ app.get('/', function (req, res) {
 });
 
 
-/*app.get('/todos', function (req, res) {
-    var queryParams = req.query;
-    var filteredTodos = todos;
 
-    if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'true') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: true
-        });
-    } else if (queryParams.hasOwnProperty('completed') && queryParams.completed == 'false') {
-        filteredTodos = _.where(filteredTodos, {
-            completed: false
-        });
-    }
-
-    if (queryParams.hasOwnProperty('q') && queryParams.q.length > 0) {
-        filteredTodos = _.filter(filteredTodos, function (todo) {
-            return todo.description.toLowerCase().indexOf(queryParams.q.toLowerCase()) > -1;
-        });
-    }
-
-    res.json(filteredTodos);
-});*/
 
 
 app.get('/todos', function(req, res){
@@ -152,22 +131,6 @@ app.post('/todos', function (req, res) {
     }, function(e){
         res.status(400).json(e);
     });
-
-    /*if (!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0) {
-        return res.status(400).send("No Todo Found!");
-    }
-
-
-    body.description = body.description.trim();
-
-
-    body.id = todoNextId++;
-
-    todos.push(body);
-
-    console.log('description: ' + body.description);
-
-    res.json(body);*/
 });
 
 db.sequelize.sync().then(function () {
