a7a5a9301ed65485260df73805ca4364e06ace9b
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-18 00:18:46 +0000

    Added Parameter Precision

--- server.js
+++ server.js
@@ -1,5 +1,6 @@
 var express = require('express');
 var bodyParser = require('body-parser');
+var _ = require('underscore');
 
 
 
@@ -25,13 +26,7 @@ app.get('/todos', function(req, res){
 
 app.get('/todos/:id', function(req, res){
     var todoId = parseInt(req.params.id);
-    var matchedTodo;
-    
-    todos.forEach(function(todo){
-        if(todoId === todo.id){
-            matchedTodo = todo;
-        }
-    });
+    var matchedTodo = _.findWhere(todos, {id: todoId});
     
     if(matchedTodo){
         res.json(matchedTodo);
@@ -43,7 +38,15 @@ app.get('/todos/:id', function(req, res){
 });
 
 app.post('/todos', function(req, res){
-    var body = req.body;
+    var body = _.pick(req.body,"description", "completed");
+    
+    if(!_.isBoolean(body.completed) || !_.isString(body.description) || body.description.trim().length === 0)     {
+        return res.status(400).send();
+    }
+    
+    
+    body.description = body.description.trim();
+    
     
     body.id = todoNextId++;
     
