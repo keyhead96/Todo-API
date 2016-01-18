72eac5cf150677509d35416772f42dcf63c8e873
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-18 00:59:54 +0000

    Added Update Functionality

--- server.js
+++ server.js
@@ -55,6 +55,43 @@ app.delete('/todos/:id', function(req, res){
 });
 
 
+app.put('/todos/:id', function(req, res){
+    var todoId = parseInt(req.params.id, 10);
+    var matchedTodo = _.findWhere(todos, {id: todoId});
+    var body = _.pick(req.body,"description", "completed");
+    var validAttributes = {};
+    
+    
+    if(!matchedTodo){
+        return res.status(404).send();
+    }
+    
+    
+    if(body.hasOwnProperty('completed') && _.isBoolean(body.completed)){
+        validAttributes.completed = body.completed;
+    } else if(body.hasOwnProperty('completed')){
+        return res.status(400).send();
+    } else {
+        //Never Provided Attribute, No Problems
+    }
+    
+    if(body.hasOwnProperty('description') && _.isString(body.description) && body.description.length > 0){
+        validAttributes.description = body.description;
+    } else if(body.hasOwnProperty('description')){
+        return res.status(400).send();
+    } else {
+        //Never Provided, No Problems
+    }
+    
+    _.extend(matchedTodo, validAttributes);
+    
+    res.json(matchedTodo);
+    
+    
+});
+
+
+
 app.post('/todos', function(req, res){
     var body = _.pick(req.body,"description", "completed");
     
