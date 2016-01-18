294710f8faf8f63e0e563a9671b61cdb8488e36f
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-18 05:22:43 +0000

    Added Sequelize

---
+++ SQLite/basic-sqlite-database.js
@@ -0,0 +1,71 @@
+var Sequelize = require('sequelize');
+var sequelize = new Sequelize(undefined, undefined, undefined, {
+    'dialect': 'sqlite',
+    'storage': __dirname + '/basic-sqlite-database.sqlite'
+});
+
+var Todo = sequelize.define('todo', {
+    description: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        validate: {
+            len: [1, 250]
+        }
+    },
+    completed: {
+        type: Sequelize.BOOLEAN,
+        allowNull: false,
+        defaultValue: false
+    }
+});
+
+
+sequelize.sync().then(function () {
+    /*force: true*/
+    }).then(function () {
+        console.log('Everything is Synced.');
+    
+        Todo.findById(2).then(function(todo){
+            if(todo){
+             console.log(todo.toJSON());   
+            } else {
+                console.log('Todo Not Found.');
+            }
+             
+        });
+       
+    
+    
+    
+        /*Todo.create({
+            description: 'Walk My Dog',
+
+        }).then(function (todo) {
+
+        return Todo.create({
+            description: "Clean Office"
+
+        });
+
+    }).then(function () {
+        return Todo.findById(1);
+        return Todo.findAll({
+            where: {
+                description: {
+                    $like: '%office%'
+                }
+            }
+        });
+    }).then(function (todos) {
+        if (todos) {
+            todos.forEach(function(todo){
+                console.log(todo.toJSON());
+            });
+        } else {
+            console.log('No Todo Found!');
+        }
+    }).catch(function (e) {
+        console.log(e);
+    });*/
+    
+});
