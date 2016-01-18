8328ded9b93bfe6156ffa440013a1a2b5e62197a
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-18 01:30:46 +0000

    Added Sort Functionalities

---
+++ variable-playground.js
@@ -0,0 +1,23 @@
+/*
+var person = {
+    name: "Kyle",
+    age: 19
+};
+
+function updatePerson(obj){
+    obj.age = 20;
+}
+
+updatePerson(person);
+
+console.log(person);
+*/
+
+
+//Array Example
+
+var grades = [14, 37];
+
+function addGrades(grades){
+    grades.push(55);
+}
