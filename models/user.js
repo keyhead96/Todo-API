0ec7c72a2dbb98d5d7a1027310d2b4c1a2ae617e
Kyle O'Connor <kyleoconnor5000@gmail.com>
2016-01-19 02:08:26 +0000

    Added User Functionality

---
+++ models/user.js
@@ -0,0 +1,38 @@
+module.exports = function(sequelize, DataTypes) {
+    return sequelize.define('todo', {
+        description: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [1, 250]
+            }
+        },
+        completed: {
+            type: DataTypes.BOOLEAN,
+            allowNull: false,
+            defaultValue: false
+        }
+    });
+
+};
+
+module.exports = function(sequelize, DataTypes){
+    return sequelize.define('user', {
+        email: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            unique: true,
+            validate: {
+                isEmail: true
+            }
+        },
+        password: {
+            type: DataTypes.STRING,
+            allowNull: false,
+            validate: {
+                len: [7, 100]
+            }
+        }
+    });
+}
+
